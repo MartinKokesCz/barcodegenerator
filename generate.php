c2e16582f82d4e89abd3798a75c6adae53bcf749
John Pilar <pilarjan4111@gmail.com>
2019-12-17 12:34:46 +0100

    Another EAN

--- generate.php
+++ generate.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'header.php';
 use setasign\Fpdi\Fpdi;
+
 // TODO check if it is in array 500 1000 2000
 $amount = filter_input(INPUT_GET, 'amount');
 $EAN = filter_input(INPUT_GET, 'EAN');
@@ -8,27 +9,80 @@ $EAN = filter_input(INPUT_GET, 'EAN');
 ?>
 <div class="container">
     <?php
-    
+    $generatorHTML = new Picqer\Barcode\BarcodeGeneratorHTML();
+    //echo $generatorHTML->getBarcode($EAN, $generatorHTML::TYPE_EAN_13, 1);
 
-    $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
-    echo $generator->getBarcode($EAN, $generator::TYPE_CODE_128);
+    $generatorPNG = new Picqer\Barcode\BarcodeGeneratorPNG();
+    $file = $generatorPNG->getBarcode($EAN, $generatorPNG::TYPE_EAN_13, 1);
+    file_put_contents("test.png", $file);
     ?>
 </div>
 <?php
 
 
-$pdf = new Fpdi();
-$pdf->AddPage('L', array(22.86,11.7602));
+(!file_exists('output')) ? mkdir('output') : null;
+(!file_exists('staticEAN')) ? mkdir('staticEAN') : null;
+
+define('SIZE', array(228.6, 117.602));
 $filepath = 'assets' . DIRECTORY_SEPARATOR . $amount . '.pdf';
 
-$pageCount = $pdf->setSourceFile($filepath);
-$tplIdx = $pdf->importPage(1);
-// use the imported page and place it at position 10,10 with a width of 100 mm
-$pdf->useTemplate($tplIdx, 0, 0, 228.6);
-$pdf->SetFont('Helvetica');
-$pdf->SetTextColor(255, 0, 0);
-$pdf->SetXY(30, 30);
-$pdf->Write(0, 'This is just a simple text');
-$pdf->Output('F', "output" . DIRECTORY_SEPARATOR . "output2.pdf");
+
+$pdf = new Fpdi();
+
+$pdf->AddPage('L', SIZE);
+$pdf->setSourceFile($filepath);
+$tplId1 = $pdf->importPage(1);
+$pdf->useTemplate($tplId1, 0, 0);
+
+$pdf->AddPage('L', SIZE);
+$tplId2 = $pdf->importPage(2);
+$pdf->useTemplate($tplId2, 0, 0);
+
+
+$pdf->SetFont('Courier', null, 10);
+$pdf->SetTextColor(0, 0, 0);
+$pdf->SetXY(20, 96);
+$pdf->Text(12.5, 108, $EAN);
+$pdf->Image('test.png', 14, 96);
+
+
+$staticEAN = array('500' => '2011000086145', '1000' => '2011000086121', '2000' => '2011000861469');
+
+$amountEAN = $generatorPNG->getBarcode($staticEAN[$amount], $generatorPNG::TYPE_EAN_13, 1);
+file_put_contents("staticEAN/$amount.png", $amountEAN);
+
+
+switch ($amount) {
+    case 500:
+        $pdf->Image('staticEAN/500.png', 190, 96);
+        $pdf->Text(189, 108, $staticEAN[$amount]);
+        break;
+    
+    case 1000:
+        $pdf->Image('staticEAN/1000.png', 190, 96);
+        $pdf->Text(189, 108, $staticEAN[$amount]);
+        break;
+
+    case 2000:
+        $pdf->Image('staticEAN/2000.png', 190, 96);
+        $pdf->Text(189, 108, $staticEAN[$amount]);
+        break;
+
+    default:
+        throw new Exception("Invalid amount!", 1);
+        break;
+}
+
+
+$pdf->Output('F', "output" . DIRECTORY_SEPARATOR . "output.pdf");
+
+?>
+<div class="text-center mt-5">
+    <img src="staticEAN/<?= $amount ?>.png" alt="" class="src">
+<div>
+<?php
+
+
+
 
 require_once 'footer.php';
